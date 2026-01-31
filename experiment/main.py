157045723bfd74f2bc1023eb56554c34313ee526
JuanLiee <juanliexx@gmail.com>
2026-01-31 09:36:28 +0700

    finished

---
+++ experiment/main.py
@@ -0,0 +1,14 @@
+def sum_then_print(*numbers):
+    print(numbers)
+    
+    total = 0
+    for n in numbers:
+        print("This is total: ", total)
+        print("This is number: ", n)
+        total = total + n
+        print("Current total: ", total)
+    return(total)
+
+result = sum_then_print(2, 3, 4, 5, 4)
+print(result)
+# output ➜ 18
